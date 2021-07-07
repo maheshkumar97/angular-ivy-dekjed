72da479123b303df592688c3c94fb4305c4ff7bb
maheshkumar97 <gonemaheshkumar@gmail.com>
2021-07-07 13:51:04 +0530

    Initial commit

---
+++ src/first-component/first-component.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements OnInit {
+  name = 'Maheshkumar';
+  constructor() {}
+
+  ngOnInit() {}
+}
